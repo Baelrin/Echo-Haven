a308497401fadf69105707229c955a86f9cc7ef3
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-25 07:22:17 +0300

    Refactore code and add class

--- master.py
+++ master.py
@@ -16,30 +16,30 @@ async def get_home_page(request: Request):
         "notes_count": len(notes_list.all_notes)
     })
 
-@app.post("/create_note")
-async def send_notes(note_data: Note):
-    # Генерируем ID записки
-    note_id = get_note_id(text=note_data.text, salt=note_data.secret)
-    note_data.note_hash = note_id
-    notes_list.all_notes.append(note_data)
-    return {"response": "ok", "note_id": note_id}
 
-@app.get("/result/{note_id}", response_class=HTMLResponse)
-async def get_result_id(request: Request, note_id: str):
-    return templates.TemplateResponse("hash_storage.html", {
-        "request": request,
-        "note_id": note_id
-    })
+class NoteManager:
+    """Class responsible for managing notes."""
 
-@app.post("/get_note")
-async def get_note(note_data: NoteID):
-    for note in notes_list.all_notes:  # по уже добавленным запискам
-        if (note_data.note_id == note.note_hash and
-            note_data.note_secret == note.secret):
-                note_index = notes_list.all_notes.index(note)
-                notes_list.all_notes.pop(note_index)
+    def __init__(self):
+        """Initialize the NoteManager with an empty notes list."""
+        self.notes_list = NotesList()
+
+    def create_note(self, note_data: Note):
+        """Create a new note and add it to the notes list."""
+        note_id = get_note_id(text=note_data.text, salt=note_data.secret)
+        note_data.note_hash = note_id
+        self.notes_list.all_notes.append(note_data)
+        return {"response": "ok", "note_id": note_id}
+
+    def get_note(self, note_data: NoteID):
+        """Get a note from the notes list by its ID and secret."""
+        for note in self.notes_list.all_notes:
+            if (note_data.note_id == note.note_hash and
+                    note_data.note_secret == note.secret):
+                note_index = self.notes_list.all_notes.index(note)
+                self.notes_list.all_notes.pop(note_index)
                 return {"response": "ok", "note_final_text": note.text}
-    return {"response": "ok", "note_final_text": "Such a note does not exist"}
+        raise ValueError("Such a note does not exist")
 
 @app.get("/note_page/{note_text}", response_class=HTMLResponse)
 async def get_result_note(request: Request, note_text: str):
--- schemes.py
+++ schemes.py
@@ -1,12 +1,16 @@
 from pydantic import BaseModel
+from typing import Optional
+
 
 class Note(BaseModel):
     text: str
     secret: str
-    note_hash: str = None  # Результат, ссылка на готовую записку
+    note_hash: Optional[str] = None  # Результат, ссылка на готовую записку
+
 
 class NotesList(BaseModel):
-    all_notes: list[Note] = list()
+    all_notes: list[Note] = []
+
 
 class NoteID(BaseModel):
     note_id: str
